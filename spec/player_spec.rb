7adb466f9e985d7fbea5629c89384cc88dbaaff3
Leander Karp <leander.karp@invision.de>
2020-09-02 12:43:32 +0200

    Added player base.

---
+++ spec/player_spec.rb
@@ -0,0 +1,11 @@
+require 'player'
+
+RSpec.describe Player do 
+  subject(:player) {described_class.new(name, icon)}
+
+  describe '#make_move' do 
+    
+  end
+  context 'name'
+  context 'icon'
+end 
