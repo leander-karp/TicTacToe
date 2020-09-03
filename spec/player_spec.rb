ab12058dd05b3d05587aa71a9027bdedf29e85aa
Leander Karp <leander.karp@invision.de>
2020-09-03 15:24:16 +0200

    add player name and icon.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,23 @@
 require 'game_base/player'
 
 RSpec.describe Player do 
-  subject(:player) {described_class.new(name, precedence)}
+  subject(:player) {described_class.new(name, icon)}
 
-  describe '#make_move' do 
-    
+  context 'at construction' do
+    let(:name) do 
+      'Max'
+    end 
+    let(:icon) do 
+      'O'
+    end
+
+    it 'has a name' do 
+      expect(player.name).to eq 'Max'
+    end
+
+    it 'has an icon' do 
+      expect(player.icon).to eq 'O'
+    end
   end
-  context 'name'
-  context 'precedence'
+
 end 
