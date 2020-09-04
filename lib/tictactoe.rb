815f6e9ffefbe73f76bb8a10e0d95509bbf6881d
Leander Karp <leander.karp@invision.de>
2020-09-04 08:41:16 +0200

    Add board attribute and improved make_move

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,5 +1,14 @@
+require 'game_base/board'
+
 class TicTacToe 
-  def make_move(player, position)
-    true 
+  attr_reader :board
+
+  def initialize
+    @board = Board.new(3,3)
   end
+    
+  def make_move(player, position)
+    @board.add(Piece.new(player.icon), position)
+  end
+
 end
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -8,20 +8,35 @@ RSpec.describe TicTacToe do
   subject(:playerA) {Player.new('Max', 'X')}
   subject(:playerB) {Player.new('Moritz', 'O')}
 
-describe '#start'
+  describe '#start'
   describe '#game_over?' do 
     it 'equals true if there are 3 in a row' do 
       board = Board.new
       board.add(Piece.new('X'), [1,1])
     end 
   end 
+
+  describe '#board' do 
+    it 'equals a valid 3x3 tic tac toe board' do 
+      expect(game.board.number_of_rows).to eq 3 
+      expect(game.board.number_of_colums).to eq 3
+    end 
+  end
+
   describe '#winner'
   describe '#current_player'
   describe '#make_move' do 
-    context 'when given a player and a valid position' do 
-      it 'equals true' do 
-        expect(game.make_move(playerA, [1,1])).to be true 
+    context 'when given a player and a position' do 
+      it 'sets a Piece and returns true' do 
+        expect(game.make_move(playerA, [1,1])).to be true
+        expect(game.board.get(1,1)).to eq Piece.new(playerA.icon) 
       end 
+
+      it 'equals false if the position is occupied' do 
+        game.make_move(playerA, [1,1])
+        expect(game.make_move(playerB, [1,1])).to be false 
+        expect(game.board.get(1,1)).to eq Piece.new(playerA.icon) 
+      end
     end
   end
 end 
