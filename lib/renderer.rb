edc8077de5481bc93199d2def7ede6b7ef220f38
Leander Karp <leander.karp@invision.de>
2020-09-09 15:37:34 +0200

    Add #print_board and improve #input

--- lib/renderer.rb
+++ lib/renderer.rb
@@ -3,28 +3,32 @@ require_relative 'game/player'
 require_relative 'game/piece'
 
 class Renderer
-  def input(game)
+  def self.input(game)
     puts game.current_player.name + ' enter your next move:'
+    input = gets.chomp.split('').map do |char|
+      num = char.to_i
+      num if num.to_s == char
+    end.compact
+
+    input.slice(0, 2) unless input.length < 2
   end
 
-  
- ##### 
-  def XXXinput(game)
-    puts 'Enter the board position you like to occupy:'
-    input = gets.strip.chomp.split('')
-    
-    loop do 
-      p input
-      row_index = input.length==2? input.first.to_i : -2
-      column_index = input.length==2? input.last.to_i : -2
-      puts column_index, row_index
-      success = game.make_move(game.current_player, [row_index, column_index])
-      p success
-      break if success
+  def self.print_board(game)
+    board_string = []
+    border = '+-+-+-+'
 
-      puts 'Invalid input'
-      input = gets.strip.chomp.split('')
-    end 
-  end 
+    for row_index in 0...game.board.rows
+      row_data = ['']
+      for column_index in 0...game.board.columns 
+        piece = game.board.get(row_index, column_index)
+        row_data.push(piece.nil? ? ' ' : piece.value)
+      end 
+      board_string.push(border)
+      row_data.push('')
+      board_string.push(row_data.join('|'))
+    end
+    board_string.push(border)
+    puts board_string
+  end
 end
 
--- spec/renderer_spec.rb
+++ spec/renderer_spec.rb
@@ -1,4 +1,4 @@
-require 'renderer'
+require 'described_class'
 require 'tictactoe'
 require 'game/player'
 require 'game/piece'
@@ -8,16 +8,52 @@ RSpec.describe Renderer do
   let(:playerB) {Player.new('Moritz', 'X')}
 
   let(:game) {TicTacToe.new(playerA, playerB)}
-  let(:renderer) {described_class.new}
 
   describe '#input' do
     it 'addresses the current player' do 
       msg = game.current_player.name + " enter your next move:\n"
-
-      expect{renderer.input(game)}.to output(msg).to_stdout
+      allow(described_class).to receive(:gets).and_return('')
+      expect{described_class.input(game)}.to output(msg).to_stdout
     end 
 
-    
+    it 'retrieves a valid position and returns it as an array of integers' do 
+      allow(described_class).to receive(:gets).and_return('11')
+      expect(described_class.input(game)).to eq [1,1]
+    end
+
+    it 'equals nil if the input is empty' do 
+      allow(described_class).to receive(:gets).and_return('')
+      expect(described_class.input(game)).to eq nil
+    end 
+
+    it 'equals nil if there is only one char' do 
+      allow(described_class).to receive(:gets).and_return('1')
+      expect(described_class.input(game)).to eq nil
+    end 
+
+    it 'equals an array of the size 2 if the input is longer' do 
+      allow(described_class).to receive(:gets).and_return('123')
+      expect(described_class.input(game).size).to eq 2
+    end 
+
+    it 'equals nil if the input is 1a' do 
+      allow(described_class).to receive(:gets).and_return('1a')
+      expect(described_class.input(game)).to eq nil
+    end
 
   end 
+
+  describe '#print_board' do 
+    it 'prints the empty board' do 
+      expected_board = "+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n"
+      expect{described_class.print_board(game)}.to output(expected_board).to_stdout
+    end
+
+    it 'prints the board correctly after a move was made' do 
+      expected_board = "+-+-+-+\n|O| | |\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n"
+      game.make_move(game.current_player, [0,0])
+      expect{described_class.print_board(game)}.to output(expected_board).to_stdout
+    end
+    # muss auf die Textbreite achten -> Board#colum_at
+  end
 end
